2bb0241e7fda760a774167d3966fe20bd91bbddd
Mohamed Azzain <m.alzain248@gmail.com>
2022-08-11 09:38:28 +0200

    Change Email Demo Model
    Change how the fields will be displayed regarding their lable texts and organizations. The field lable will be displaying the corresponding partner name, and they will be organized in two columns.

--- practice/models/email_demo.py
+++ practice/models/email_demo.py
@@ -1,15 +1,36 @@
 
 from odoo import api, fields, models
 
-class SaleOrder(models.Model):
+
+class EmailDemo(models.Model):
     _name = 'email.demo'
     _inherit = ['mail.thread']
     _description = "Email Demo"
 
-    partner_id = fields.Many2one(comodel_name='res.partner', string="Receiver", required=True)
-    random_check_1 = fields.Boolean(string="Random Check 1")
-    random_check_2 = fields.Boolean(string="Random Check 2")
+    res_partner_1 = fields.Boolean(string="Partner 1")
+    res_partner_2 = fields.Boolean(string="Partner 2")
+    res_partner_3 = fields.Boolean(string="Partner 3")
+    res_partner_4 = fields.Boolean(string="Partner 4")
+
+    @property
+    def receivers_selection_map(self):
+        # These partners are already in the system from demo data,
+        # You should check they exists in your system,
+        # Or you can replace them by others
+        return {'res_partner_1': self.env.ref('base.res_partner_1'),
+               'res_partner_2': self.env.ref('base.res_partner_2'),
+               'res_partner_3': self.env.ref('base.res_partner_3'),
+               'res_partner_4': self.env.ref('base.res_partner_4')}
 
     def action_send_mail(self):
-        # Do whatever logic you have with these random_check_1, random_check_1 and if you decide to send the email execute the next line.
-        self.message_post(body='Hello, From the email demo. <br/> Email Demo body', subject='email demo subject', partner_ids=self.partner_id.ids)
+        selected_partners = [p.id for k, p in self.receivers_selection_map.items() if self[k]]
+        self.message_post(body='Hello, From the email demo. <br/> Email Demo body',
+                          subject='email demo subject', partner_ids=selected_partners)
+
+    # To display the fields by partner names in the form
+    @api.model
+    def fields_get(self, **kwargs):
+        result = super().fields_get(**kwargs)
+        for k in self.receivers_selection_map:
+            result[k]['string'] = self.receivers_selection_map[k].display_name
+        return result
